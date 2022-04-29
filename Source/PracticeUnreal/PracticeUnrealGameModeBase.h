c3febc4d8e3c3744275bf1a76cf3b67f82a23d47
Stiner <stiner01@gmail.com>
2022-04-29 12:15:32 +0900

    Import project

---
+++ Source/PracticeUnreal/PracticeUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PracticeUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICEUNREAL_API APracticeUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
