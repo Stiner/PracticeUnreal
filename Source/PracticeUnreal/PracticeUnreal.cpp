f078fdd4f96eddd82d49cca9afce61276e558ccc
Stiner <stiner01@gmail.com>
2022-08-25 21:09:24 +0900

    프로젝트 리셋

--- Source/PracticeUnreal/PracticeUnreal.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "PracticeUnreal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PracticeUnreal, "PracticeUnreal" );
