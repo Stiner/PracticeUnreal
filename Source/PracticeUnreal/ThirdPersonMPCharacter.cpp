8ce1c433abbbf0402becc33fb7716a8c0652f602
Stiner <stiner01@gmail.com>
2022-12-16 15:16:27 +0900

    Base Class 변경

---
+++ Source/PracticeUnreal/ThirdPersonMPCharacter.cpp
@@ -0,0 +1,112 @@
+#include "ThirdPersonMPCharacter.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/Engine.h"
+#include "MPRifle.h"
+
+AThirdPersonMPCharacter::AThirdPersonMPCharacter()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    MaxHealth = 100.0f;
+    CurrentHealth = MaxHealth;
+}
+
+void AThirdPersonMPCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(AThirdPersonMPCharacter, CurrentHealth);
+}
+
+float AThirdPersonMPCharacter::TakeDamage(float damageTaken, FDamageEvent const& damageEvent, AController* eventInstigator, AActor* damageCauser)
+{
+    float damageApplied = CurrentHealth - damageTaken;
+    SetCurrentHealth(damageApplied);
+    return damageApplied;
+}
+
+void AThirdPersonMPCharacter::SetCurrentHealth(float healthValue)
+{
+    if (GetLocalRole() == ROLE_Authority)
+    {
+        CurrentHealth = FMath::Clamp(healthValue, 0.0f, MaxHealth);
+        OnHealthUpdate();
+    }
+}
+
+void AThirdPersonMPCharacter::OnRep_CurrentHealth()
+{
+    OnHealthUpdate();
+}
+
+void AThirdPersonMPCharacter::OnHealthUpdate()
+{
+    if (IsLocallyControlled())
+    {
+        FString healthMsg = FString::Printf(TEXT("You now have %f health remaining."), CurrentHealth);
+        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, healthMsg);
+
+        if (CurrentHealth <= 0)
+        {
+            FString deadMsg = FString::Printf(TEXT("You have been killed."));
+            GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, deadMsg);
+        }
+    }
+
+    if (GetLocalRole() == ROLE_Authority)
+    {
+        FString healthMsg = FString::Printf(TEXT("%s now has %f health remaining."), *GetName(), CurrentHealth);
+        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, healthMsg);
+    }
+
+    // TOOD
+}
+
+void AThirdPersonMPCharacter::EquipWeapon_Implementation(AMPRifle* equipWeapon)
+{
+    if (IsValid(equipWeapon))
+    {
+        if (IsValid(equipWeapon->Owner))
+            return;
+        if (IsValid(Weapon))
+            return;
+
+        Weapon = equipWeapon;
+
+        Weapon->SetOwner(this);
+    }
+}
+
+void AThirdPersonMPCharacter::UnequipWeapon_Implementation()
+{
+    if (IsValid(Weapon))
+    {
+        Weapon->SetOwner(nullptr);
+        Weapon = nullptr;
+    }
+}
+
+void AThirdPersonMPCharacter::StartUseWeapon()
+{
+    if (IsValid(Weapon))
+    {
+        Weapon->StartFire();
+
+        StartUseWeapon_Server();
+    }
+}
+
+void AThirdPersonMPCharacter::OnEndUseWeapon()
+{
+    OnEndUseWeapon_Server();
+}
+
+void AThirdPersonMPCharacter::StartUseWeapon_Server_Implementation()
+{
+    UsingWeapon = true;
+}
+
+void AThirdPersonMPCharacter::OnEndUseWeapon_Server_Implementation()
+{
+    UsingWeapon = false;
+}
---
+++ Source/PracticeUnreal/ThirdPersonMPProjectile.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ThirdPersonMPProjectile.generated.h"
+
+UCLASS()
+class PRACTICEUNREAL_API AThirdPersonMPProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AThirdPersonMPProjectile();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		class USphereComponent* SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadonly, category = "Components")
+		class UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadonly, Category = "Components")
+		class UProjectileMovementComponent* ProjectileMovementComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Effects")
+		class UParticleSystem* ExplosionEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Damage")
+		TSubclassOf<class UDamageType> DamageType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Damage")
+		float Damage;
+
+protected:
+	virtual void Destroyed() override;
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Projectile")
+		void OnProjectileImpact(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OthreComp, FVector NormalImpulse, const FHitResult& Hit);
+    virtual void OnProjectileImpact_Implementation(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OthreComp, FVector NormalImpulse, const FHitResult& Hit);
+
+};
