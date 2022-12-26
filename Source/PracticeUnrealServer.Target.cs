e553c441e5ec2bac7076a85b4692dca9108e8693
Stiner <stiner01@gmail.com>
2022-12-26 16:53:32 +0900

    서버 타겟 설정 추가

---
+++ Source/PracticeUnrealServer.Target.cs
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+using System;
+
+public class PracticeUnrealServerTarget : TargetRules
+{
+    public PracticeUnrealServerTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Server;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+
+        ExtraModuleNames.AddRange(new string[] { "PracticeUnreal" });
+    }
+}
